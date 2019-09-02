e8ff9849f544c199171c0e95604c49db8ddc98eb
Rubens Leme <rubenss.leme@gmail.com>
2019-09-02 20:23:02 -0300

    Inicio do projeto

---
+++ projetoModelo_A/app/src/main/java/br/com/exemplo/projetomodelo_a/TelaMenu.java
@@ -0,0 +1,14 @@
+package br.com.exemplo.projetomodelo_a;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TelaMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_menu);
+    }
+}
